2aae43cfc466afde101624e5e4b95649a84c65be
Raghav Gupta <raghav@gpp.co.in>
2023-01-19 16:43:19 +0530

    Created test.py file, and added .idea file to gitignore

---
+++ test.py
@@ -0,0 +1,23 @@
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1*n2
+
+
+def divide(n1, n2):
+    return
+
+
+n1 = input()
+n2 = input()
+
+print(add(n1, n2))
+print(subtract(n1, n2))
+print(multiply(n1, n2))
+print(divide(n1, n2))
